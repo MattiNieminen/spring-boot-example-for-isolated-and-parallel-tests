aa8562c6d1ad39d07c4305e510580c7f738c9761
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2025-10-07 22:49:09 +0300

    Add project created with Spring Initializr

---
+++ src/main/java/com/example/spring_boot_example_for_isolated_and_parallel_tests/SpringBootExampleForIsolatedAndParallelTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_example_for_isolated_and_parallel_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleForIsolatedAndParallelTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleForIsolatedAndParallelTestsApplication.class, args);
+	}
+
+}
