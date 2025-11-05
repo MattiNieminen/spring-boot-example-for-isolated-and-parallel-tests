82a8afe2a67ade1ebc54c40080b84e28748fe9c0
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2025-11-05 21:25:40 +0200

    Add slow, database using, non-isolated (and therefore failing) example tests

---
+++ app/src/test/java/com/example/spring_boot_example_for_isolated_and_parallel_tests/ExampleTestA.java
@@ -0,0 +1,27 @@
+package com.example.spring_boot_example_for_isolated_and_parallel_tests;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ExampleTestA {
+
+	@Autowired
+	private PlanetRepository planetRepository;
+
+	@Test
+	void testA() {
+		try { Thread.sleep(5000); } catch (InterruptedException e) {}
+		Planet planet = new Planet("Mars", 6779);
+		assertThat(planetRepository.findAll()).hasSize(0);
+		planetRepository.save(planet);
+		List<Planet> planets = planetRepository.findAll();
+		assertThat(planets).hasSize(1);
+		assertThat(planets.getFirst().getName()).isEqualTo(planet.getName());
+	}
+}
